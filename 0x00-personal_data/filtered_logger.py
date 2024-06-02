aedcb2c4efc20edc7d82fed2e979cae51ba44ce9
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2024-06-02 06:14:34 +0300

    feat: Implement a get_logger function that takes no arguments and returns a logging.Logger object

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,6 +7,9 @@ from typing import List
 import logging
 
 
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
+
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """
@@ -43,3 +46,14 @@ class RedactingFormatter(logging.Formatter):
         """ Formater method that returns the log message obfuscated"""
         return filter_datum(self.fields, self.REDACTION,
                             super().format(record), self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    """ Get logger method"""
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(stream_handler)
+    return logger
