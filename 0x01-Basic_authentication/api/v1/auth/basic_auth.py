1321ec92371b0d0a9e689bd20c629c9a09b820c4
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2024-06-04 02:01:18 +0300

    feat: Add the method 'extract_base64_authorization_header' to extract credentials from the request header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,4 +7,12 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """ Basic authentication handler class """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str
+                                            ) -> str:
+        """ Method to extract the base64 part of the Authorization header """
+        if not authorization_header or type(authorization_header) is not str:
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header.strip('Basic ')
