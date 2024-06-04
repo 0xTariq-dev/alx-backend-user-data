6d7d61510631e75ab5b689c51f237a1b75fb1273
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2024-06-04 21:41:03 +0300

    feat: Add extract_user_credentials method for BasicAuth class

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -4,29 +4,30 @@ Route module for the API
 """
 import os
 from os import getenv
-from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
+
+from api.v1.views import app_views
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
+auth_type = getenv("AUTH_TYPE", "auth")
+if auth_type == 'auth':
+    auth = Auth()
+if auth_type == 'basic_auth':
+    auth = BasicAuth()
 
 
 @app.before_request
 def before_request() -> None:
     """ Before request handler
     """
-    auth = getenv("AUTH_TYPE")
     if auth:
-        if auth == 'basic_auth':
-            from api.v1.auth import basic_auth as ba
-            auth = ba.BasicAuth()
-        else:
-            from api.v1.auth.auth import Auth
-            auth = Auth()
         if auth.require_auth(request.path, ['/api/v1/status/',
                                             '/api/v1/unauthorized/',
                                             '/api/v1/forbidden/']):
--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,8 @@ Module of authentication views
 """
 from api.v1.auth.auth import Auth
 from base64 import b64decode
+from typing import Tuple, TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -29,3 +31,28 @@ class BasicAuth(Auth):
             return b64decode(base64_authorization_header).decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header: str
+                                 ) -> Tuple[str, str]:
+        """ Method to extract user credentials """
+        if not decoded_base64_authorization_header or \
+                type(decoded_base64_authorization_header) is not str or \
+                ':' not in decoded_base64_authorization_header:
+            return (None, None)
+        return tuple(decoded_base64_authorization_header.split(':', 1))
+
+    def user_object_from_credentials(self,
+                                     user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """ Method to get a user object from credentials """
+        if type(user_email) == str and type(user_pwd) == str:
+            try:
+                users = User.search({'email': user_email})
+            except Exception:
+                return None
+            if len(users) <= 0:
+                return None
+            if users[0].is_valid_password(user_pwd):
+                return users[0]
+        return None
